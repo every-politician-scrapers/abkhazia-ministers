e2c32954328d8ccbfc0b1748605b33fd2ab491c9
Tony Bowden <tony@tmtm.com>
2022-02-20 13:34:51 +0200

    compare enwiki Foreign Ministers

---
+++ sources/foreign-minister/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no img name start end].freeze
+    end
+
+    def raw_end
+      super.gsub('†', '').gsub(/\(.*\)/, '').tidy
+    end
+
+    def empty?
+      tds[1].text == tds[2].text
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
